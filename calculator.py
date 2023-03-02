d36cf1455c473fae84438f53106772ea89d0c336
Dennis Boyd <dboyd1@gmail.com>
2023-03-02 12:50:01 -0500

    Updated calculator.py and added art.

--- calculator.py
+++ calculator.py
@@ -1,36 +1,40 @@
-# A simple calculator
+# A simple text-based calculator
+# An art.py module containing ASCII art for the logo
+from art import logo
 
-ADDITION_SELECTION = 1
-SUBTRACTION_SELECTION = 2
-MULTIPLICATION_SELECTION = 3
-DIVISION_SELECTION = 4
+ADDITION_SELECTION = "1"
+SUBTRACTION_SELECTION = "2"
+MULTIPLICATION_SELECTION = "3"
+DIVISION_SELECTION = "4"
 
 
-# Function to add two numbers
+# This function takes in two variables to add them.
 def add(n1, n2):
     return n1 + n2
 
 
-# Function to subtract two numbers
+# This function takes in two variables and subtracts the second from the first.
 def subtract(n1, n2):
     return n1 - n2
 
 
-# Function to multiply two numbers
+# This function takes in two variables and multiplies them.
 def multiply(n1, n2):
     return n1 * n2
 
 
-# Function to divide two numbers
+# This function takes in two variables and divides the first by the second.
 def divide(n1, n2):
     return n1 / n2
 
 
-print("Calculator Menu -\n" \
-      "1. Add two numbers -\n" \
-      "2. Subtract two numbers -\n" \
-      "3. Multiply two numbers -\n" \
-      "4. Divide two numbers -\n")
+# The directions for the user are displayed using simple print statements along with the logo.
+print(logo)
+print("Calculator Menu")
+print("1. Add two numbers")
+print("2. Subtract two numbers")
+print("3. Multiply two numbers")
+print("4. Divide two numbers")
 
 # Takes user input in the form of a string to select the operation.
 select = input("Which operation would you like to carry out? Pick a number 1-4: ")
@@ -41,19 +45,15 @@ number2 = int(input("Enter the second number: "))
 
 # Prints and carries out the chosen operation and prints the output.
 if select == ADDITION_SELECTION:
-    print(number1, " +", number2, "=",
-          add(number1, number2))
+    print(f"{number1} + {number2} = {add(number1, number2)}")
 
 elif select == SUBTRACTION_SELECTION:
-    print(number1, "-", number2, "=",
-          subtract(number1, number2))
+    print(f"{number1} - {number2} = {subtract(number1, number2)}")
 
 elif select == MULTIPLICATION_SELECTION:
-    print(number1, "*", number2, "=",
-          multiply(number1, number2))
+    print(f"{number1} * {number2} = {multiply(number1, number2)}")
 
 elif select == DIVISION_SELECTION:
-    print(number1, "/", number2, "=",
-          divide(number1, number2))
+    print(f"{number1} / {number2} = {divide(number1, number2)}")
 else:
     print("Invalid input!")
